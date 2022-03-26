b364edd3b40596bf8df502a96fad6b4151c2f82e
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-03-26 23:43:08 +0530

    :sparkles: added username in dashboard

--- packages/web/src/components/common/Navbar.tsx
+++ packages/web/src/components/common/Navbar.tsx
@@ -5,6 +5,8 @@ import Typography from "@material-ui/core/Typography";
 import React, { SyntheticEvent } from "react";
 import useAuth from "../../hooks/useAuth";
 import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ApplicationState } from "../../store";
 
 const useStyles = makeStyles({
   root: {
@@ -18,6 +20,10 @@ const Navbar = () => {
   const classes = useStyles();
   const history = useHistory();
   const [isAuth, handleLogout] = useAuth();
+
+  const fullName = useSelector(
+    (state: ApplicationState) => state.user.fullName
+  );
 
   const handleRedirect = (event: SyntheticEvent, tag: string) => {
     event.preventDefault();
@@ -49,6 +55,7 @@ const Navbar = () => {
               Signup
             </Button>
           )}
+          {isAuth && <Typography color="inherit">{fullName}</Typography>}
           {isAuth && (
             <Button onClick={() => handleLogout()} color="inherit">
               Logout
